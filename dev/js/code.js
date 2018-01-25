13b4d7540323a79340e82219cf3e9bddfb6a6c8e
Coopert <kupchynskyi@gmail.com>
2018-01-25 09:46:38 +0200

    changes

--- dev/js/code.js
+++ dev/js/code.js
@@ -1,9 +1,12 @@
 $(function(){
-	$(window).resize(function() {
-	var height = $('.header-pic').height()
-	console.log(height)
-	var difference = 388-height
-	bottom = 388-difference
-	$('.header-info').css("bottom",bottom)
-	})
+	var browserMinWidth = $(document).width()
+	if (browserMinWidth > 800) {
+		$(window).resize(function() {
+		var height = $('.header-pic').height()
+		console.log(height)
+		var difference = 388-height
+		bottom = 388-difference
+		$('.header-info').css("bottom",bottom)
+		})
+	}
 });
